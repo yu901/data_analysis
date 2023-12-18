40cefab561b749bb85ec7173acb1d546c09d9236
이유진 <yu901@puzzledata.com>
2023-12-18 17:20:23 +0900

    kofis 영화 일별 데이터 추출

---
+++ src/main/python/config.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+
+class BaseConfig:
+    def __init__(self):
+        self.root_path = os.environ.get("ROOT_PATH", ".")
+        self.set_yml_path("/src/main/python/config.yml")
+        if not os.path.exists(self.yml_path):
+            encrypt_yml_path = "/src/main/python/config.yml"
+            print(f"{self.yml_path} does not exist. use {encrypt_yml_path}")
+            self.set_yml_path(encrypt_yml_path)
+
+        with open(self.yml_path, "r", encoding="utf8") as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+
+    def set_yml_path(self, yml_file_name):
+        config_path = os.environ.get("CONFIG_PATH", yml_file_name)
+        self.conf_dir = config_path
+        self.yml_path = self.root_path + self.conf_dir
+
+
+class KofisConfig(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        storage = self.config["kofis"]
+        self.key = storage["key"]
---
+++ src/main/python/movie.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import pandas as pd
+import datetime
+from tqdm import tqdm
+from config import KofisConfig
+
+kofis_config = KofisConfig()
+
+class Movie():
+    def __init__(self):
+        self.key = kofis_config.key
+
+    def get_extract_range(self, startDt, period):
+        f = "%Y%m%d"
+        start_date = datetime.datetime.strptime(startDt, f)
+        extract_range = []
+        for days in range(period):
+            extract_date = start_date + datetime.timedelta(days=days)
+            if extract_date < datetime.datetime.now():
+                extract_date = extract_date.strftime(f)
+                extract_range.append(extract_date)
+            else:
+                breakpoint
+        return extract_range
+
+    def request_BoxOffice(self, targetDt):
+        url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+        params = {
+            "key": self.key,
+            "targetDt": targetDt
+        }    
+        while True:
+            try:
+                response = requests.get(url, params=params)
+                text = response.text
+                loads = json.loads(text)
+                df = pd.DataFrame(loads["boxOfficeResult"]["dailyBoxOfficeList"])
+            except:
+                continue
+            break
+        return df
+
+    def get_BoxOffice(self, startDt, period):
+        extract_range = self.get_extract_range(startDt, period)
+        boxoffice_df = pd.DataFrame()
+        for extract_date in tqdm(extract_range):
+            df = self.request_BoxOffice(extract_date)
+            df["targetDt"] = extract_date[:4] + "-" + extract_date[4:6] + "-" + extract_date[6:]
+            boxoffice_df = pd.concat([boxoffice_df, df], ignore_index=True)
+        col_types = {
+            "salesAmt": "float",
+            "salesShare": "float",
+            "salesInten": "float",
+            "salesChange": "float",
+            "salesAcc": "float",
+            "audiCnt": "float",
+            "audiInten": "float",
+            "audiChange": "float",
+            "audiAcc": "float",
+            "scrnCnt": "float",
+            "showCnt": "float",
+            "openDt": "datetime64[ns]",
+            "targetDt": "datetime64[ns]",
+        }
+        boxoffice_df = boxoffice_df.astype(col_types)
+        return boxoffice_df
+    
+
+if __name__ == '__main__':
+    movie = Movie()
+    df = movie.get_BoxOffice("20231122", 20)
+    print(df)
